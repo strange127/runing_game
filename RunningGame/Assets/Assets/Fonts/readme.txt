54147d3c6bc0cac55e03692c00c9bba614cba763
galasticcoders <93988862+galasticcoders@users.noreply.github.com>
2021-11-09 23:14:57 +0530

    mainMenuUI
    UI ADDED

---
+++ RunningGame/Assets/Assets/Fonts/readme.txt
@@ -0,0 +1,14 @@
+sports tfb.ttf is a free for personal use ..... =)
+
+contains 93 silhouettes of athletes
+
+direct link to the font http://truefonts.blogspot.com/2012/11/siluetas-de-deportes.html
+
+
+Thanks for download
+Gracias por descargar
+
+
+my site: http://truefonts.blogspot.com
+
+Kaiserzharkhan - Chile
---
+++ RunningGame/Assets/Assets/Level.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+ public void ModesMenu() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadStartMenu() {
+        SceneManager.LoadScene("Start Menu");
+    }
+
+ public void LoadSettingsMenu() {
+     SceneManager.LoadScene("Settings");
+ }
+
+ public void QuitGame() {
+     Application.Quit();
+ }
+
+ public void LoadLast() {
+     SceneManager.LoadScene("End Game");
+ }
+
+ public void LoadOnlineMenu() {
+     SceneManager.LoadScene("Online mode");
+ }
+
+void Update() {
+ if(Input.GetKeyDown(KeyCode.P)) {
+     SceneManager.LoadScene("Pause Menu");
+ }
+}
+
+
+ 
+}
