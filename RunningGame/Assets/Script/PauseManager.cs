8ebe27326cb66ba73656d11a4f24a6b9ccfcd0f2
Giga_sat <90619014+Scientillionaire@users.noreply.github.com>
2021-11-22 13:05:01 +0530

    pause menu

--- RunningGame/Assets/Script/PauseManager.cs
+++ RunningGame/Assets/Script/PauseManager.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
+    [SerializeField] GameObject pauseMenu;
 
     public void PauseGame()
     {
-        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
     }
     public void Resume()
+    {   
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+     }
+    public void Gameplay(int SceneID)
     {
-        Time.timeScale = 1;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneID);
     }
 }
